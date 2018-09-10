e02e970843116029f84312a492acdb7a806621cd
Kimathi <kiprotich.kaai@gmail.com>
2018-09-10 09:39:19 -0400

    Testing out Object Destructuring and the creation of Object id's in mongoDB

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
 // Last slash is the database to connect to. Can create a new one
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
   if(err){
     return console.log('Unable to connect to MongoDB server');
   }
@@ -18,16 +23,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'John Smith',
-    age: 25,
-    location: '123 Big St. W'
-  }, (err, result) => {
-    if(err) {
-      return console.log('Unable to insert user',err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'John Smith',
+  //   age: 25,
+  //   location: '123 Big St. W'
+  // }, (err, result) => {
+  //   if(err) {
+  //     return console.log('Unable to insert user',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
 
   client.close(); //Closes connection with the mongodb server
 });
