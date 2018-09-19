f0454aa29e8b2779e96dbcd64a5445d5ee4d3e69
Kimathi <kiprotich.kaai@gmail.com>
2018-09-19 14:26:49 -0400

    Object ID validation

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp',{db: {
+      readPreference: "secondaryPreferred",
+    }});
 
 module.exports = {mongoose};
