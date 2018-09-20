219f079bc1d8815de3a4f0dbb3ed6b9e02f000ee
Kimathi <kiprotich.kaai@gmail.com>
2018-09-19 23:16:01 -0400

    Setup seperate test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{db: {
-      readPreference: "secondaryPreferred",
-    }});
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
